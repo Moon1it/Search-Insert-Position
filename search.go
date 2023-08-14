4b508b8153ce95ced6bfbb1c5cc1f605461f5bd0
Kirill Smirnov <62110814+Moon1it@users.noreply.github.com>
2023-08-14 14:52:50 +0200

    SearchInsert

---
+++ search.go
@@ -0,0 +1,31 @@
+package main
+
+func SearchInsert(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+	mid := 0
+
+	for low <= high {
+		mid = (high + low) / 2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	if nums[mid] > target {
+		return mid
+	} else {
+		return mid + 1
+	}
+}
+
+func main() {
+
+}
---
+++ search_test.go
@@ -0,0 +1,59 @@
+package main
+
+import "testing"
+
+func TestSearchInsert(t *testing.T) {
+
+	testTable := []struct {
+		nums     []int
+		target   int
+		expected int
+	}{
+		{
+			nums:     []int{1, 3, 5, 6},
+			target:   5,
+			expected: 2,
+		},
+		{
+			nums:     []int{1, 3, 5, 6},
+			target:   2,
+			expected: 1,
+		},
+		{
+			nums:     []int{1, 3, 5, 6},
+			target:   6,
+			expected: 3,
+		},
+		{
+			nums:     []int{1, 3, 5, 6, 12, 14},
+			target:   0,
+			expected: 0,
+		},
+		{
+			nums:     []int{1, 3, 5, 6, 10, 12, 14, 43, 54},
+			target:   15,
+			expected: 7,
+		},
+		{
+			nums:     []int{3, 5, 6, 14, 43, 54, 142, 155},
+			target:   80,
+			expected: 6,
+		},
+		{
+			nums:     []int{3, 5, 6, 7, 14, 16, 43, 54, 67, 142, 155},
+			target:   46,
+			expected: 7,
+		},
+	}
+
+	for _, testCase := range testTable {
+
+		result := SearchInsert(testCase.nums, testCase.target)
+		t.Logf("Calling SearchInsert(%d, %d), result: %d\n", testCase.nums, testCase.target, testCase.expected)
+
+		if result != testCase.expected {
+			t.Errorf("Incorrect result. Expect %d, got %d\n\n", testCase.expected, result)
+		}
+	}
+
+}
